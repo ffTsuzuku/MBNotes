3b268bd362f1c01348194d103c3f817f339d3fc7
Deep <gagandeepsandhu@protonmail.com>
2025-02-16 00:04:01 -0500

    refactor: Updated model methods to return actual model instances

--- api/models/Model.ts
+++ api/models/Model.ts
@@ -1,22 +1,27 @@
 import fs from 'node:fs'
 
 export default abstract class  Model {
-	static readonly table: string
-	static readonly primaryKey = 'id'
+	protected  static readonly table: string
+	protected  static primaryKey = 'id'
+
+	protected  original: Record<string, any>
+	protected  attributes: Record<string, any>
 
 	static timestamps = true
 	static created_at = 'created_at'
 	static deleted_at = 'created_at'
 
-	constructor(attributes: Partial<Model>){
-		Object.assign(this, attributes)
+
+	constructor(attributes: Record<string, any>){
+		this.attributes = {...attributes}
+		this.original = {...attributes}
 	}
 
 	static model_name () {
 		return this.name
 	}
 
-	private static get_db() {
+	protected  static get_db() {
 		const ROOT_DIR = process.env.APP_ROOT_DIR 
 		if (!ROOT_DIR) {
 			console.log(process.env)
@@ -27,16 +32,22 @@ export default abstract class  Model {
 		return JSON.parse(file.toString())
 	}
 
-	private static get_table<T extends Model>(): T[] {
-		return this.get_db()[this.table]
+	protected  static get_table(): Record<string, any>[] {
+		return this.get_db()[this.table] ?? []
 	}
 
-	static async all () {
-		this.get_table()
+	static async all(): Promise<Model[]> {
+		const table = this.get_table()
+		//@ts-ignore
+		return table.map(record => new this(record))
 	}
 
-	static async find<T extends Model> ( id: number): Promise<T|undefined> {
-		const records = this.get_table<T>()
-		return records.find(record => record[this.primaryKey] === id)
+	static async find<T extends Model> (id: number): Promise<T|undefined> {
+		const records = this.get_table()
+		const record = records.find(record => record[this.primaryKey] === id) 
+		//@ts-ignore
+		if(record) return new this(record)
+
+		return undefined
 	}
 }
