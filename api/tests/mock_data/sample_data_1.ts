cbb0d7c10b9fb4a9c03fe736a8ccb0ee67d0b9e2
Deep <gagandeepsandhu@protonmail.com>
2025-02-24 01:07:46 -0500

    assigned_to test data added to sample data

--- api/tests/mock_data/sample_data_1.ts
+++ api/tests/mock_data/sample_data_1.ts
@@ -5,6 +5,7 @@ export default {
       {
         "id": 1,
         "title": "no fake stuff",
+		"assigned_to": "Mark",
         "created_at": "2025-02-17T00:00:00.000Z",
         "updated_at": "2025-02-18T17:53:13-05:00",
         "deleted_at": null
@@ -12,6 +13,7 @@ export default {
       {
         "id": 2,
         "title": "MEOW",
+		"assigned_to": "Mark",
         "created_at": "2025-02-17T00:00:00.000Z",
         "updated_at": "2025-02-17T01:23:41-05:00",
         "deleted_at": "2025-02-17T00:00:00.000Z"
@@ -19,12 +21,14 @@ export default {
       {
         "title": "MEOW",
         "id": 3,
+		"assigned_to": "Jim",
         "created_at": "2025-02-17T01:24:38-05:00",
         "updated_at": "2025-02-17T01:24:38-05:00"
       },
       {
         "title": "UPDATE TEST",
         "id": 7,
+		"assigned_to": "Sarah",
         "created_at": "2025-02-17T01:38:17-05:00",
         "updated_at": "2025-02-17T01:38:17-05:00",
         "deleted_at": "17-02-2025"
@@ -32,12 +36,14 @@ export default {
       {
         "title": "UPDATE TEST",
         "id": 8,
+		"assigned_to": "Zoe",
         "created_at": "2025-02-17T01:40:59-05:00",
         "updated_at": "2025-02-17T01:40:59-05:00",
         "deleted_at": "17-02-2025"
       },
       {
         "title": "SHOULDNT BE ADDED",
+		"assigned_to": "Ahri",
         "created_at": "2025-02-18T00:41:40-05:00",
         "updated_at": "2025-02-18T00:41:40-05:00",
         "deleted_at": null,
@@ -45,6 +51,7 @@ export default {
       },
       {
         "title": "SHOULDNT BE ADDED",
+		"assigned_to": null, 
         "created_at": "2025-02-18T00:42:42-05:00",
         "updated_at": "2025-02-18T00:42:42-05:00",
         "deleted_at": null,
@@ -52,6 +59,7 @@ export default {
       },
       {
         "title": "SHOULDNT BE ADDED",
+		"assigned_to": null, 
         "created_at": "2025-02-20T00:42:42-05:00",
         "updated_at": "2025-02-20T00:42:42-05:00",
         "deleted_at": null,
@@ -59,6 +67,7 @@ export default {
       },
       {
         "title": "Milk",
+		"assigned_to": null, 
         "created_at": "2025-02-20T00:42:42-05:00",
         "updated_at": "2025-02-20T00:42:42-05:00",
         "deleted_at": null,
@@ -66,6 +75,7 @@ export default {
       },
       {
         "title": "Pineapple",
+		"assigned_to": null, 
         "created_at": "2025-02-20T00:42:42-05:00",
         "updated_at": "2025-02-20T00:42:42-05:00",
         "deleted_at": null,
@@ -74,6 +84,7 @@ export default {
       {
         "title": "Apple iPhone 15",
         "created_at": "2025-02-20T00:42:42-05:00",
+		"assigned_to": null, 
         "updated_at": "2025-02-20T00:42:42-05:00",
         "deleted_at": null,
         "id": 47
