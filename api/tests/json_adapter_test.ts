b88c1d04d8e902ba9c1eac6437a26c930c3cfc09
Deep <gagandeepsandhu@protonmail.com>
2025-02-24 01:11:39 -0500

    added more test for query builder

--- api/tests/json_adapter_test.ts
+++ api/tests/json_adapter_test.ts
@@ -1,7 +1,7 @@
-import {assertEquals, assertGreater } from 'jsr:@std/assert'
+import {assertEquals, assertGreater, assertThrows } from 'jsr:@std/assert'
 import { describe, it } from "jsr:@std/testing/bdd";
 import QueryBuilder from '../core/db/QueryBuilder.ts';
-import {make_mock_json_db_adapter} from '../core/utility/test.ts';
+import {make_mock_json_db_adapter, make_mock_query} from '../core/utility/test.ts';
 
 const JSONDBAdapterProxy = make_mock_json_db_adapter()
 
@@ -129,6 +129,17 @@ describe({name: 'test like where clause', ignore: false}, () => {
 		const all_records = QueryBuilderProxy.from('tickets') .get()
 
 		assertEquals(records.length, all_records.length)
+	})
+
+	it({name: 'test like against num val', ignore: false}, () => {
+		const QueryBuilderProxy = new QueryBuilder(
+			'tickets', JSONDBAdapterProxy
+		)
+		const record = QueryBuilderProxy.from('tickets')
+			.where('id', 'like', 1)
+			.get()
+
+		assertEquals(record.length, 1)
 	})
 })
 
@@ -312,3 +323,120 @@ describe({name: 'test not rlike where clause', ignore: false}, () => {
 		assertGreater(titles_ending_with_apple.length, 0)
 	})
 })
+
+describe({name: 'test whereIn clause', ignore: false}, () => {
+	it({name: 'where shouldnt include bad ids', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+		const desirable_ticket_ids = [1,2,3,9]
+		const records = QueryBuilder.whereIn('id', desirable_ticket_ids).get()
+
+		const tickets_with_wrong_id = records.filter(record => {
+			const id_set = new Set(desirable_ticket_ids)
+			return !id_set.has(record.id)
+		})
+
+		assertEquals(tickets_with_wrong_id.length, 0)
+	})
+	
+	it({name: 'includes the right ids', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+		const desirable_ticket_ids = [1,2,3,9]
+		const records = QueryBuilder.whereIn('id', desirable_ticket_ids).get()
+
+		const tickets_with_wrong_id = records.filter(record => {
+			const id_set = new Set(desirable_ticket_ids)
+			return id_set.has(record.id)
+		})
+
+		assertEquals(tickets_with_wrong_id.length, 3)
+	})
+})
+
+describe({name: 'test whereNull', ignore: false}, () => {
+	it({name: 'all records have null deleted-at', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+
+		const records = QueryBuilder.table('tickets')
+			.whereNull('deleted_at')
+			.get()
+
+		const valid_records = records.filter(
+			record => record.deleted_at === null
+		)
+
+		assertGreater(valid_records.length, 0)
+	})
+
+	it({name: 'all records have null deleted-at v2', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+
+		const records = QueryBuilder.table('tickets')
+			.whereNull('deleted_at')
+			.get()
+
+		const valid_records = records.filter(
+			record => record.deleted_at !== null
+		)
+
+		assertEquals(valid_records.length, 0)
+	})
+})
+
+describe({name: 'multiple wheres', ignore: false}, () => {
+	it({name: 'whereIn and whereNull', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+		const desirable_ticket_ids = [1,2,3,9]
+		const records = QueryBuilder
+			.whereIn('id', desirable_ticket_ids)
+			.whereNull('deleted_at')
+			.get()
+
+		const tickets_with_wrong_conditions = records.filter(record => {
+			const id_set = new Set(desirable_ticket_ids)
+			return !id_set.has(record.id) || record.deleted_at !== null
+		})
+
+		assertEquals(tickets_with_wrong_conditions.length, 0)
+	})
+
+	it({name: 'whereIn and whereNull', ignore: false}, () => {
+		const QueryBuilder = make_mock_query('tickets')
+		const desirable_ticket_ids = [1,2,3,9]
+		const records = QueryBuilder
+			.whereIn('id', desirable_ticket_ids)
+			.orWhere('title', 'like', '%milk%')
+			.get()
+
+
+		const tickets_with_wrong_conditions = records.filter(record => {
+			const id_set = new Set(desirable_ticket_ids)
+			return !id_set.has(record.id) ||
+				!record.title.toLowerCase().includes('milk')
+		})
+
+		assertEquals(tickets_with_wrong_conditions.length, 4)
+	})
+})
+
+describe({name: 'test db value is null', ignore: false}, () => {
+	it({name: 'like clause', ignore: false}, () => {
+		const QueryBuilderProxy = new QueryBuilder(
+			'tickets', JSONDBAdapterProxy
+		)
+		const records = QueryBuilderProxy.from('tickets')
+			.where('assigned_to', 'like', '%sarah%')
+			.get()
+		
+		assertEquals(records.length, 1)
+	})
+})
+
+Deno.test({
+	name: 'Error is thrown when select non-existent column',
+	ignore: false 
+}, () => {
+	const QueryBuilder = make_mock_query('Ticket')
+	const fn = () => QueryBuilder.select(['lied']).get()
+	assertThrows(fn)
+})
+
